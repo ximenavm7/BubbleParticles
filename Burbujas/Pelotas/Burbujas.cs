8ca83aa2e8100a9d3b61d70aa0bf32c3468c6365
ximenavm7 <122324007+ximenavm7@users.noreply.github.com>
2023-02-23 01:00:54 -0600

    Add files via upload

---
+++ Burbujas/Pelotas/Burbujas.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using System.Runtime.Remoting.Channels;
+
+namespace Pelotas
+{
+    public class Burbujas
+    {
+        int index;
+        Size space;
+        public Color c;
+        // Variables de posición
+        public float x;
+        public float y;
+
+        // Variables de velocidad
+        private float vx;
+        private float vy;
+
+        // Variable de radio
+        public float radio;
+
+
+        // Constructor
+        public Burbujas(Random rand,Size size, int index)
+        {
+
+            this.radio  = rand.Next(15, 20);
+            this.x      = rand.Next((int)radio, size.Width - (int)radio);
+            this.y      = size.Height - radio;         
+            c           = Color.FromArgb(rand.Next(200, 255), rand.Next(250, 255), rand.Next(235, 255), rand.Next(240, 255));
+
+            // Velocidades iniciales
+            this.vx = rand.Next(1, 5);
+            this.vy = rand.Next(-6,-1);
+
+            this.index = index;
+            space = size;
+        }
+
+        // Método para actualizar la posición de la burbuja en función de su velocidad
+        public void Update(float deltaTime, List<Burbujas> balls, Random rand, Size size)
+        { 
+            if ((y - radio) <= 0)
+            {
+                vy = rand.Next(-6, -1);
+                y = size.Height - radio;
+                x = rand.Next((int)radio, size.Width - (int)radio);
+            }
+
+            if ((x - radio) <= 0 || (x + radio) >= space.Width)
+            {
+                if (x - radio <= 0)
+                    x = radio + 3;
+                else
+                    x = space.Width - radio-3;
+                    
+                //vx *= -.01f;
+                vy *= .5f;
+            }
+            
+            if ((y - radio) <= 0 || (y + radio) >= space.Height)
+            {
+                if (y - radio<=  0)
+                    y = radio + 3;
+                else
+                    y = space.Height - radio-3;
+
+                
+                //vx *=  .01f;
+                vy *= -.5f;
+            }
+
+            //this.x += this.vx + .55f; // * deltaTime;
+            this.y -= this.vy + 4; // * deltaTime;
+        }
+    }
+
+}
---
+++ Burbujas/Pelotas/Particulas.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using Pelotas.Properties;
+using System.Reflection.Emit;
+
+namespace Pelotas
+{
+    public partial class Particulas : Form
+    {
+        static List<Burbujas> balls;
+        static Bitmap bmp = Resources.diver;
+        static Graphics g;
+        static Random rand = new Random();
+        static float deltaTime;
+
+        public Particulas()
+        {
+            InitializeComponent();
+        }
+
+        private void Init()
+        {
+            if (PCT_CANVAS.Width == 0)
+                return;
+            balls       = new List<Burbujas>();
+            bmp         = new Bitmap(PCT_CANVAS.Width, PCT_CANVAS.Height);
+            g           = Graphics.FromImage(bmp);
+            deltaTime   = 1;
+            PCT_CANVAS.Image = bmp;
+            
+
+            for (int b = 0; b < 50; b++)
+            {
+                balls.Add(new Burbujas(rand, PCT_CANVAS.Size, b));
+            }
+        }
+
+        private void Pelotas_Load(object sender, EventArgs e)
+        {
+            Init();
+            this.BackgroundImage = Resources.diver;
+        }
+
+        private void Pelotas_SizeChanged(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void TIMER_Tick(object sender, EventArgs e)
+        {
+            g.DrawImage(this.BackgroundImage, 0, 0, this.Width, this.Height);
+            Parallel.For(0, balls.Count, b =>//ACTUALIZAMOS EN PARALELO
+            {
+                Burbujas P;
+                balls[b].Update(deltaTime, balls, rand, PCT_CANVAS.Size);
+                P = balls[b];               
+            });
+
+            Burbujas p;
+
+            for (int b = 0; b < balls.Count; b++)//PINTAMOS EN SECUENCIA
+            {
+                p = balls[b];
+
+                // Dibujar la burbuja del tamaño de la pelota
+                g.DrawImage(Resources.bubble, p.x - p.radio, p.y - p.radio, 2 * p.radio, 2 * p.radio);
+            }
+
+
+            PCT_CANVAS.Invalidate();
+            deltaTime += .1f;
+        }
+    }
+}
